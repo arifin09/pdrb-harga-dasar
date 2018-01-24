c823c62a8f3fcb7fe9f6c4dada098371c66b6b19
Feri Demons <32523275+feripratama@users.noreply.github.com>
2018-01-24 19:59:11 +0700

    modify model
    fix namespace

--- src/Models/Bantenprov/PdrbHargaDasar/Province.php
+++ src/Models/Bantenprov/PdrbHargaDasar/Province.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\Bantenprov\PdrbHargaDasar;
+namespace Bantenprov\PdrbHargaDasar\Models\Bantenprov\PdrbHargaDasar;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;

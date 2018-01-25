f2bb0817a2b660e8b63e765aa69b4864bbc4453d
feripratama <eroorsys@gmail.com>
2018-01-25 17:40:00 +0700

    update echarts base on year  On branch master  Changes to be committed: 	modified:   README.md 	modified:   src/public/json/bantenprov/pdrb-harga-dasar/pdrb-harga-dasar02.json 	modified:   src/resources/assets/js/components/views/bantenprov/pdrb-harga-dasar/PdrbHargaDasarBar02.vue 	modified:   src/resources/assets/js/components/views/bantenprov/pdrb-harga-dasar/PdrbHargaDasarPie02.vue

--- src/resources/assets/js/components/views/bantenprov/pdrb-harga-dasar/PdrbHargaDasarPie02.vue
+++ src/resources/assets/js/components/views/bantenprov/pdrb-harga-dasar/PdrbHargaDasarPie02.vue
@@ -31,7 +31,7 @@ export default {
         visualMap: {
           show: false,
           min: 1000,
-          max: 80000,
+          max: 300000,
           inRange: {
             colorLightness: [0, 1]
           }
@@ -42,6 +42,7 @@ export default {
             radius : '55%',
             center: ['50%', '50%'],
             data:[
+                {value:0, name:''},
                 {value:0, name:''},
                 {value:0, name:''},
                 {value:0, name:''},
@@ -64,8 +65,8 @@ export default {
                   color: '#eee'
                 },
                 smooth: 0.2,
-                length: 10,
-                length2: 11
+                length: 20,
+                length2: 40
               }
             },
             itemStyle: {
@@ -76,8 +77,8 @@ export default {
               }
             },
 
-            animationType: false,
-            animationEasing: false,
+            animationType: 'scale',
+            animationEasing: 'cubicOut',
             animationDelay: function (idx) {
               return Math.random() * 200;
             }
@@ -87,45 +88,10 @@ export default {
     }
   },
   mounted: function () {
-    /*axios.get('/json/bantenprov/pdrb-harga-dasar/data.json').then(response => {
-      let i = 0;
-      for(var first = 0; first < Object.keys(response.data[0].chartdata.grafik[0].tahun[0]).length; first++){
-        this.pie.series[0].data[first].value = Object.values(response.data[0].chartdata.grafik[0].tahun[0])[first]
-        this.pie.series[0].data[first].name = Object.keys(response.data[0].chartdata.grafik[0].tahun[0])[first]
-        this.pie.title.text = response.data[0].chartdata.grafik[0].tingkat + ' ' + response.data[0].chartdata.grafik[0].name
-      }
 
-      this.pie.visualMap.max = Math.max.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0])) + 2000
-      this.pie.visualMap.min = Math.min.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0])) - 2000
-
-      setInterval(() => {
-        i++;
-        setTimeout(() => {
-          for(var k = 0; k < Object.keys(response.data[0].chartdata.grafik[0].tahun[0]).length; k++){
-            this.pie.series[0].data[k].value = Object.values(response.data[0].chartdata.grafik[i].tahun[0])[k]
-            this.pie.series[0].data[k].name = Object.keys(response.data[0].chartdata.grafik[i].tahun[0])[k]
-
-            this.pie.title.text = response.data[0].chartdata.grafik[i].tingkat + ' ' + response.data[0].chartdata.grafik[i].name
-
-            this.pie.visualMap.max = Math.max.apply(null,Object.values(response.data[0].chartdata.grafik[i].tahun[0])) + 6000
-            this.pie.visualMap.min = Math.min.apply(null,Object.values(response.data[0].chartdata.grafik[i].tahun[0])) - 6000
-          }
-        }, 1000);
-
-        if(i ==  response.data[0].chartdata.grafik.length) {
-          i = 0;
-        }
-      }, 5000);
-
-    })
-    .catch(function(error) {
-      // error
-    });
-  }*/
-
-
-  axios.get('/json/bantenprov/pdrb-harga-dasar/pdrb-harga-dasar02.json').then(response => {
-      //let i = 0;
+    axios.get('/json/bantenprov/pdrb-harga-dasar/pdrb-harga-dasar02.json').then(response => {
+      let obj_key = [];
+      var datas = response.data;
 
       function removeDuplicates(arr){
         var unique_array = []
@@ -136,218 +102,39 @@ export default {
         }
         return unique_array
       }
-      //console.log(Object.values(response.data[0].chartdata.grafik[0].tahun[0]))
 
-      /*for(var name = 0; name < response.data[0].chartdata.grafik.length; name++)
-      {
-        if(response.data[0].chartdata.grafik[name].tingkat == "dua"){
-          this.pie.series[0].data[name].name = response.data[0].chartdata.grafik[name].name
-        }
-      }*/
+      // set nilai awal
 
-      /*console.log(response.data[0].chartdata.grafik[0].tahun)
-      console.log(response.data[0].chartdata.grafik.length)*/
+      Object.values(datas[0])[0].forEach((data, index)=>{
+        this.pie.series[0].data[index].name   = data.wilayah + ' ' + data.name + ' - ' + data.data.toLocaleString('EN')
+        this.pie.series[0].data[index].value  = data.data
+        this.pie.title.text = 'Tahun ' + Object.keys(datas[0])[0]
+      })
 
-      var tahun_get = [];
-      for(var tahun = 0; tahun < response.data[0].chartdata.grafik.length; tahun++)
-      {
-        tahun_get.push(response.data[0].chartdata.grafik[tahun].tahun)
-        //console.log(response.data[0].chartdata.grafik[0].tahun[])
-      }
+      var i = 1;
 
-      let tahun_filter = removeDuplicates(tahun_get)
-      let i = 0;
+      setInterval(()=>{
+        Object.values(datas[0])[i].forEach((data, index) => {
+            this.pie.series[0].data[index].name   = data.wilayah + ' ' + data.name + ' - ' + data.data.toLocaleString('EN')
+            this.pie.series[0].data[index].value  = data.data
+            this.pie.title.text = 'Tahun ' + Object.keys(datas[0])[i]
+
+        });
 
 
-      console.log(tahun_filter)
+        i++;
 
-      // this.pie.series[0].data[0].value = 2000
-      // this.pie.series[0].data[0].name = "test"
-      //console.log(response.data[0].chartdata.grafik.tahun)
-      //var tahun = [];
-
-      for(let graph1 = 0; graph1 < response.data[0].chartdata.grafik.length; graph1++)
-      {
-        //console.log(response.data[0].chartdata.grafik[graph1].tahun)
-        //console.log((response.data[0].chartdata.grafik[graph1].tahun === tahun_filter[0]))
-        if(response.data[0].chartdata.grafik[graph1].tahun == tahun_filter[0] && response.data[0].chartdata.grafik[graph1].tingkat == "dua")
+        if(i == Object.keys(datas[0]).length)
         {
-
-          /*this.pie.series[0].data[graph1].value = response.data[0].chartdata.grafik[graph1].data
-          this.pie.series[0].data[graph1].name = response.data[0].chartdata.grafik[graph1].name*/
-
-          this.pie.series[0].data[i].value = response.data[0].chartdata.grafik[graph1].data
-          this.pie.series[0].data[i].name = response.data[0].chartdata.grafik[graph1].name
-          this.pie.title.text = response.data[0].chartdata.grafik[graph1].tahun
-          console.log(response.data[0].chartdata.grafik[graph1].name)
-          i++;
+          i = 0;
         }
+      },4000)
 
-      }
-
-
-
-      let loop_interval = 0;
-
-      setInterval(() => {
-
-        setTimeout(() => {
-          var i_interval = 1;
-          for(let graph2 = 0; graph2 < response.data[0].chartdata.grafik.length; graph2++)
-          {
-
-            if(response.data[0].chartdata.grafik[graph2].tahun == tahun_filter[loop_interval] && response.data[0].chartdata.grafik[graph2].tingkat == "dua")
-            {
-
-              //console.log(i_interval)
-              this.pie.series[0].data[i_interval].value = response.data[0].chartdata.grafik[graph2].data
-              this.pie.series[0].data[i_interval].name = response.data[0].chartdata.grafik[graph2].name
-              this.pie.title.text = response.data[0].chartdata.grafik[graph2].tahun
-              console.log(i_interval)
-              console.log(tahun_filter[loop_interval])
-              console.log(this.pie.title.text = response.data[0].chartdata.grafik[graph2].tahun)
-              console.log(response.data[0].chartdata.grafik[graph2].name + ':' + response.data[0].chartdata.grafik[graph2].data)
-              i_interval++;
-            }
-
-
-
-          }
-
-
-        },1000)
-
-
-        loop_interval++;
-
-        if(i_interval == tahun_filter.length - 1)
-          {
-
-            i_interval = 1;
-          }
-
-        if(loop_interval ==  tahun_filter.length) {
-          loop_interval = 0;
-        }
-
-      },5000)
-
-
-      /*for(var first = 0; first < response.data[0].chartdata.grafik.length; first++){
-        if(response.data[0].chartdata.grafik[first].tingkat == "dua"){
-          this.pie.series[0].data[first].value = Object.values(response.data[0].chartdata.grafik[first].tahun[0])[0]
-        }
-        this.pie.title.text = Object.keys(response.data[0].chartdata.grafik[0].tahun[0])[0]
-      }
-      //console.log(this.pie.series[0].data)
-      // console.log(response.data[0].chartdata.grafik[0].tingkat)
-
-
-      if(response.data[0].chartdata.grafik[0].tingkat == "dua"){
-        this.pie.visualMap.max = Math.max.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0])) + 50000
-        // this.pie.visualMap.min = Math.min.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0]))
-        this.pie.visualMap.min = 1000
-      }*/
-
-      // setInterval(() => {
-      //   i++;
-
-      //   setTimeout(() => {
-
-      //     for(var name = 0; name < response.data[0].chartdata.grafik.length; name++){
-      //       for(var tahun = 0; tahun < Object.keys(response.data[0].chartdata.grafik[i].tahun[0]).length; tahun++){
-
-      //         if(i == tahun)
-      //         {
-      //           console.log(Object.keys(response.data[0].chartdata.grafik[i].tahun[0])[tahun] + ':' + Object.values(response.data[0].chartdata.grafik[i].tahun[0])[tahun])
-      //         }
-
-      //       }
-      //       // if(response.data[0].chartdata.grafik[i].tingkat == "dua"){
-      //       //   this.pie.series[0].data[name].value = Object.values(response.data[0].chartdata.grafik[i].tahun[0])[0]
-
-      //       // }
-
-      //       // //console.log(Object.values(response.data[0].chartdata.grafik[i].tahun[0])[i])
-      //       // if(response.data[0].chartdata.grafik[first].tingkat == "dua"){
-      //       //   this.pie.series[0].data[first].value = Object.values(response.data[0].chartdata.grafik[first].tahun[0])[0]
-      //       // }
-
-      //       // this.pie.title.text = Object.keys(response.data[0].chartdata.grafik[i].tahun[0])[i]
-
-      //       // /* === */
-
-      //       // if(response.data[0].chartdata.grafik[i].tingkat == "dua"){
-      //       //   this.pie.visualMap.max = Math.max.apply(null,Object.values(response.data[0].chartdata.grafik[i].tahun[0])) + 6000
-      //       //   this.pie.visualMap.min = 1000
-
-      //       // }
-
-      //       /*if(response.data[0].chartdata.grafik[0].tingkat == "dua"){
-      //         this.pie.visualMap.max = Math.max.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0])) + 50000
-      //         // this.pie.visualMap.min = Math.min.apply(null,Object.values(response.data[0].chartdata.grafik[0].tahun[0]))
-      //         this.pie.visualMap.min = 1000
-      //       }*/
-      //     }
-      //   }, 1000);
-
-      //   if(i ==  response.data[0].chartdata.grafik.length) {
-      //     i = 0;
-      //   }
-      //   //console.log(this.pie.visualMap.max)
-      // }, 5000);
 
     })
     .catch(function(error) {
       // error
     });
   }
-
-
-  /*axios.get('/json/bantenprov/pdrb-harga-dasar/pdrb-harga-dasar02.json').then(response => {
-      this.bar.title.text = 'Tahun ' + Object.keys(response.data[0].chartdata.grafik[0].tahun[0])[0]
-
-      for(var name = 0; name < response.data[0].chartdata.grafik.length; name++)
-      {
-        if(response.data[0].chartdata.grafik[name].tingkat == "dua"){
-          this.bar.xAxis.data[name] = response.data[0].chartdata.grafik[name].name;
-        }
-      }
-
-      for(var first = 0; first < response.data[0].chartdata.grafik.length; first++)
-      {
-        if(response.data[0].chartdata.grafik[first].tingkat == "dua") {
-          this.bar.series[0].data[first] = Object.values(response.data[0].chartdata.grafik[first].tahun[0])[0]
-        }
-      }
-
-      let i = 0;
-      setInterval(() => {
-        i++;
-        setTimeout(() => {
-
-          for(var k = 0; k < response.data[0].chartdata.grafik.length; k++)
-          {
-            if(response.data[0].chartdata.grafik[k].tingkat == "dua") {
-              this.bar.series[0].data[k] = Object.values(response.data[0].chartdata.grafik[k].tahun[0])[i]
-            }
-
-            this.bar.title.text = 'Tahun ' +Object.keys(response.data[0].chartdata.grafik[k].tahun[0])[i]
-
-          }
-
-
-        }, 1000);
-
-        if(i ==  Object.keys(response.data[0].chartdata.grafik[0].tahun[0]).length) {
-          i = 0;
-        }
-      }, 5000);
-    })
-    .catch(function(error) {
-      // error
-    });
-  }*/
 }
 </script>
-
